0a6a8ce1eb4b05f9e20bc20422936aa7c655d787
Chad McCallum <chadm@iqmetrix.com>
2013-08-02 11:25:12 -0600

    Added some new players

---
+++ Players/SlackBasedOnRep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Players
+{
+    public class SlackBasedOnRep : IPlayer
+    {
+        public char[] HuntChoices(int roundNumber, int currentFood, double currentReputation, int m, double[] playerReputations)
+        {
+            var result = new List<char>();
+            foreach (var rep in playerReputations)
+            {
+                result.Add(rep >= 0.5 ? 'h' : 's');
+            }
+            return result.ToArray();
+        }
+
+        public void HuntOutcomes(int[] foodEarnings)
+        {
+        }
+
+        public void RoundEnd(int award, int m, int numberOfHunters)
+        {
+        }
+    }
+}
---
+++ Players/SlackUnlessHighHunt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Players
+{
+    public class SlackUnlessHighHunt : IPlayer
+    {
+        public char[] HuntChoices(int roundNumber, int currentFood, double currentReputation, int m, double[] playerReputations)
+        {
+            var result = new List<char>();
+            for (var i = 0; i < playerReputations.Length; i++)
+            {
+                if (m > (playerReputations.Length ^ 2/2))
+                {
+                    result.Add('s');
+                }
+                else
+                {
+                    result.Add('h');
+                }
+            }
+            return result.ToArray();
+        }
+
+        public void HuntOutcomes(int[] foodEarnings)
+        {
+        }
+
+        public void RoundEnd(int award, int m, int numberOfHunters)
+        {
+        }
+    }
+}
